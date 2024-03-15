8c328c2259419fbef18fb6901427d9b07a94cc88
PUO <86181292+kl204@users.noreply.github.com>
2024-03-15 17:57:48 +0900

    OAuth2Response Naver와 Google별 클래스 개발 & SecurityCoinfig 설정

--- src/main/java/com/example/oauthjwt/config/SecurityConfig.java
+++ src/main/java/com/example/oauthjwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.oauthjwt.config;
 
+import com.example.oauthjwt.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,8 +13,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         //csrf disable
         http
                 .csrf((auth) -> auth.disable());
@@ -28,7 +37,9 @@ public class SecurityConfig {
 
         //oauth2
         http
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)));
 
         //경로별 인가 작업
         http
